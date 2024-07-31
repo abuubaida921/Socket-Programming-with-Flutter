c15dd0af8869da466fbc9a81d8de5e3fef5a7f67
Abu Ubaida <ubaidanazia@gmail.com>
2024-07-31 07:31:34 +0600

    Mobile side implemented with backend on Express JS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +12,141 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Socket',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  HomeScreenState createState() => HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeScreenState extends State<HomeScreen> {
+  late IO.Socket socket;
+  final StreamController<String> _streamController = StreamController<String>();
+  Stream<String> get messagesStream => _streamController.stream;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  TextEditingController controller = TextEditingController();
+  var usrName='';
+  var receivedData='';
+
+  //This will give platofrm specific url for ios and android emulator
+  String socketUrl() {
+    if (Platform.isAndroid) {
+      return "http://192.168.0.107:3000";
+    } else {
+      return "http://localhost:3000";
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Connect to the Socket.IO server
+    socket = IO.io(socketUrl(), <String, dynamic>{
+      'transports': ['websocket'],
+    });
+
+    socket.on('connect', (_) {
+      print('Connected to server');
+    });
+
+    // Listen for messages from the server
+    socket.on('newmsg', (data) {
+      print(data['user']);
+      print(data['message']);
+      setState(() {
+          controller.text='';
+          receivedData+="\n${data['user']}: ${data['message']}";
+
+      });
+    });
+
+    // Listen for response from the server
+    socket.on('userExists', (data) {
+      print('$data');
+      Fluttertoast.showToast(
+          msg: "$data",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      setState(() {
+        controller.text='';
+        usrName='';
+      });
+    });
+
+    socket.on('userSet', (data){
+      print(data['username']);
+      setState(() {
+        controller.text='';
+        usrName=data['username'];
+      });
     });
   }
 
   @override
+  void dispose() {
+    // Disconnect from the Socket.IO server when the app is disposed
+    socket.disconnect();
+
+    //close stream
+    _streamController.close();
+    super.dispose();
+  }
+
+  void sendMessage(String message) {
+    // Send a message to the server
+    socket.emit('msg', {'message': message, 'user': usrName});
+  }
+
+  void setUserName(String usrName) {
+    // Send a username to the server
+    socket.emit('setUsername', usrName);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Socket.IO Flutter and ExpressJS'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: TextFormField(
+                controller: controller,
+                decoration: InputDecoration(hintText: usrName==''?"Enter your new username":"Enter Message"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            TextButton(onPressed: () {
+              if (socket.connected) {
+                usrName==''?setUserName(controller.text):sendMessage(controller.text);
+              }
+            },child: Text(usrName==''?'Let me chat':'send'),),
+            const SizedBox(height: 40),
+            Text('$receivedData')
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
